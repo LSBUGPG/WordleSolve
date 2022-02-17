5b09d8aec63c30e5b23d388e54e54ca17977cc75
Paul Sinnett <paul.sinnett@gmail.com>
2022-02-17 20:09:15 +0000

    use bool array for comparison

--- Assets/Solver.cs
+++ Assets/Solver.cs
@@ -16,25 +16,17 @@ public struct Score
 
 public class WordSet
 {
-    uint [] set;
+    bool [] set;
     int count;
-
-    uint BitPattern(int bit)
-    {
-        return 0x1U << bit;
-    }
 
     public WordSet(int count, bool state)
     {
-        int size = count / 32 + 1;
-        int remainder = count % 32;
-        set = new uint [size];
+        set = new bool [count];
         int i;
-        for (i = 0; i < size - 1; ++i)
+        for (i = 0; i < count; ++i)
         {
-            set[i] = state? 0xFFFFFFFF : 0x0;
+            set[i] = state;
         }
-        set[i] = (state? 0xFFFFFFFF : 0x0) & (BitPattern(remainder) - 1);
         this.count = state? count : 0;
     }
 
@@ -43,48 +35,22 @@ public class WordSet
         int size = set.Length;
         for (int i = 0; i < size; ++i)
         {
-            uint removed = (set[i] ^ intersect.set[i]) & set[i];
-            if (removed > 0)
+            bool removed = set[i] && !intersect.set[i];
+            if (removed)
             {
-                count -= CountBits(removed);
+                count--;
+                set[i] = intersect.set[i];
             }
-            set[i] = set[i] & intersect.set[i];
         }
-    }
-
-    bool TestBit(uint pattern, int bit)
-    {
-        return ((pattern >> bit) & 0x1) != 0;
-    }
-
-    uint SetBit(uint pattern, int bit)
-    {
-        return pattern | BitPattern(bit);
     }
 
     public void Add(int index)
     {
-        int pos = index / 32;
-        int bit = index % 32;
-
-        if (!TestBit(set[pos], bit))
+        if (!set[index])
         {
-            set[pos] = SetBit(set[pos], bit);
+            set[index] = true;
             count++;
         }
-    }
-
-    int CountBits(uint pattern)
-    {
-        int bits = 0;
-        for (int bit = 0; bit < 32; ++bit)
-        {
-            if (TestBit(pattern, bit))
-            {
-                bits++;
-            }
-        }
-        return bits;
     }
 
     public int Count()
